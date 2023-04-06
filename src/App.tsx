e64b36b70091a3ddc27594ac6a10bb8293da239d
muhammadansorinasution <muhammadansorinasution@gmail.com>
2023-04-06 15:07:00 +0700

    handling error

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function App() {
+  const [success, setSuccess] = useState('')
+  const [errorState, setErrorState] = useState('')
+
   function getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
-      var innerHTML = "Geolocation is not supported by this browser.";
-      console.log(innerHTML)
+      setSuccess("Geolocation is not supported by this browser.")
     }
   }
 
   function showPosition(position: GeolocationPosition) {
-    var location = "Latitude: " + position.coords.latitude +
-      "<br>Longitude: " + position.coords.longitude;
-    console.log(location)
+    setSuccess("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude)
   }
+
+  function showError(error: GeolocationPositionError) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        setErrorState("User denied the request for Geolocation.")
+        break;
+      case error.POSITION_UNAVAILABLE:
+        setErrorState("Location information is unavailable.")
+        break;
+      case error.TIMEOUT:
+        setErrorState("The request to get user location timed out.")
+        break;
+      default:
+        setErrorState("An unknown error occurred.")
+        break;
+    }
+  }
+
   useEffect(() => {
     getLocation()
   }, [])
   return (
-    <div>App</div>
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>Success</td>
+            <td>{success}</td>
+          </tr>
+          <tr>
+            <td>Error</td>
+            <td>{errorState}</td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
   )
 }
